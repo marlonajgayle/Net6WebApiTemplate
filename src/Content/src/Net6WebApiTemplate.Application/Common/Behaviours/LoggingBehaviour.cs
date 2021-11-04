4e62657b7d87174280ddad64f964f32691f034c8
Marlon Gayle <marlon.gayle@live.com>
2021-11-04 16:47:55 -0500

    Implement MediatR behaviours  to support handling unhandled exeptions, application logging, performance logging and validation

---
+++ src/Content/src/Net6WebApiTemplate.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Net6WebApiTemplate.Application.Common.Behaviours
+{
+    public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public LoggingBehaviour(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("Net6WebApiTemplate Request: {Name} {@Request}", requestName, request);
+        }
+    }
+}
--- src/Content/src/Net6WebApiTemplate.Application/DependencyInjection.cs
+++ src/Content/src/Net6WebApiTemplate.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Net6WebApiTemplate.Application.Common.Behaviours;
 using System.Reflection;
 
 namespace Net6WebApiTemplate.Application
@@ -8,8 +10,14 @@ namespace Net6WebApiTemplate.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            // Register MediatR 
+            // Register Fluent Validation service
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Register MediatR Services
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
             return services;
         }
