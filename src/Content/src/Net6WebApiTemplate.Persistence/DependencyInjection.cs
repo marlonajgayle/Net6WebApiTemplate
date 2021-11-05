34069da09f30897168d6e056a96b663f1fae53f1
Marlon Gayle <marlon.gayle@live.com>
2021-11-05 17:20:21 -0500

    Implemented Entity Framework Core DatabaseContext

--- src/Content/src/Net6WebApiTemplate.Persistence/DependencyInjection.cs
+++ src/Content/src/Net6WebApiTemplate.Persistence/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Net6WebApiTemplate.Application.Common.Interfaces;
 
 namespace Net6WebApiTemplate.Persistence
 {
@@ -7,6 +10,17 @@ namespace Net6WebApiTemplate.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddHealthChecks()
+                .AddDbContextCheck<Net6WebApiTemplateDbContext>();
+
+            services.AddDbContext<Net6WebApiTemplateDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("Net6WebApiConnection"),
+                b => b.MigrationsAssembly(typeof(Net6WebApiTemplateDbContext).Assembly.FullName))
+                .LogTo(Console.WriteLine, LogLevel.Information)); // disable for production;
+
+            services.AddScoped<INet6WebApiTemplateDbContext>(provider =>
+                provider.GetService<Net6WebApiTemplateDbContext>());
+
             return services;
         }
     }
---
+++ src/Content/src/Net6WebApiTemplate.Persistence/Net6WebApiTemplateDbContextFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Net6WebApiTemplate.Persistence
+{
+    public class Net6WebApiTemplateDbContextFactory : IDesignTimeDbContextFactory<Net6WebApiTemplateDbContext>
+    {
+        private const string ConnectionStringName = "Net6WebApiConnection";
+        private const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+        public Net6WebApiTemplateDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Net6WebApiTemplateDbContext>();
+            optionsBuilder.UseSqlServer(GetConnectionString());
+
+            return new Net6WebApiTemplateDbContext(optionsBuilder.Options);
+        }
+
+        private static string GetConnectionString()
+        {
+            var basePath = Directory.GetCurrentDirectory();
+           
+            var environmentName = Environment.GetEnvironmentVariable(AspNetCoreEnvironment);
+            Console.WriteLine(environmentName);
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.Local.json", optional: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Console.WriteLine(configuration.GetConnectionString(ConnectionStringName));
+            var connectionString = configuration.GetConnectionString(ConnectionStringName);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException($"Connection string '{ConnectionStringName}' is null or empty.", nameof(connectionString));
+            }
+
+            return connectionString;
+        }
+    }
+}
