e6bd5afc4f74e47990a2dee7dd154488c3e9e2ee
Marlon Gayle <marlon.gayle@live.com>
2021-11-05 13:32:45 -0500

    Install EF Core nuget package and create interface for DBContext

---
+++ src/Content/src/Net6WebApiTemplate.Application/Common/Interfaces/INet6WebApiTemplateDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Net6WebApiTemplate.Domain.Entities;
+
+namespace Net6WebApiTemplate.Application.Common.Interfaces
+{
+    public interface INet6WebApiTemplateDbContext
+    {
+        DbSet<Client> Clients { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
