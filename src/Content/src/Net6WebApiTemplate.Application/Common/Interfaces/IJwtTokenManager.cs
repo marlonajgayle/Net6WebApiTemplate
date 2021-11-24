955b19b82372385497700e84d485186f00175141
Marlon Gayle <marlon.gayle@live.com>
2021-11-24 16:13:56 -0500

    Created JWT  token Manager interface

---
+++ src/Content/src/Net6WebApiTemplate.Application/Common/Interfaces/IJwtTokenManager.cs
@@ -0,0 +1,11 @@
+﻿using Net6WebApiTemplate.Application.Auth;
+using System.Security.Claims;
+
+namespace Net6WebApiTemplate.Application.Common.Interfaces
+{
+    public interface IJwtTokenManager
+    {
+        Task<AuthResult> GenerateClaimsTokenAsync(string username, CancellationToken cancellationToken);
+        Task<ClaimsPrincipal> GetPrincipFromTokenAsync(string token);
+    }
+}
