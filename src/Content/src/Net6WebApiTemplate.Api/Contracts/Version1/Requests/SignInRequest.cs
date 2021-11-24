95a685fd928738b6723565b76b1ca1d731267137
Marlon Gayle <marlon.gayle@live.com>
2021-11-24 14:58:20 -0500

    Created AuthController endpoint and SignIn request model

---
+++ src/Content/src/Net6WebApiTemplate.Api/Contracts/Version1/Requests/SignInRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Net6WebApiTemplate.Api.Contracts.Version1.Requests
+{
+    public class SignInRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
