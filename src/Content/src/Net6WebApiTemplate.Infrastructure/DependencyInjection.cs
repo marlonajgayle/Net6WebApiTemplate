41d0bd502fd85678b4c2f48e9a60e0cde3ea0c82
Marlon Gayle <marlon.gayle@live.com>
2021-11-24 16:12:41 -0500

    Add JWT configuration logic that generates  and validates tokens

--- src/Content/src/Net6WebApiTemplate.Infrastructure/DependencyInjection.cs
+++ src/Content/src/Net6WebApiTemplate.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,16 @@
 ﻿using FluentEmail.MailKitSmtp;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Net6WebApiTemplate.Application.Common.Interfaces;
 using Net6WebApiTemplate.Infrastructure.Cache.InMemory;
 using Net6WebApiTemplate.Infrastructure.DataProtection;
 using Net6WebApiTemplate.Infrastructure.Notifications.Email;
+using Net6WebApiTemplate.Infrastructure.Oauth;
+using System.Text;
 
 namespace Net6WebApiTemplate.Infrastructure
 {
@@ -14,6 +18,40 @@ namespace Net6WebApiTemplate.Infrastructure
     {
         public static IServiceCollection AddInfrastrucutre(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
         {
+            // Register OAuth services
+            services.AddTransient<IJwtTokenManager, JwtTokenManager>();
+
+            // Configure JWT Authentication and Authorization
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(nameof(JwtSettings), jwtSettings);
+            services.AddSingleton(jwtSettings);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = jwtSettings.ValidateIssuerSigningKey,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                ValidateIssuer = jwtSettings.ValidateIssuer,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidateAudience = jwtSettings.ValidateAudience,
+                ValidAudience = jwtSettings.Audience,
+                RequireExpirationTime = jwtSettings.RequireExpirationTime,
+                ValidateLifetime = jwtSettings.ValidateLifetime,
+                ClockSkew = jwtSettings.Expiration
+            };
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = tokenValidationParameters;
+            });
+
             // Register Data Protection Services
             services.AddDataProtection()
                 .SetDefaultKeyLifetime(TimeSpan.FromDays(30));
---
+++ src/Content/src/Net6WebApiTemplate.Infrastructure/Oauth/JwtTokenManager.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Net6WebApiTemplate.Application.Auth;
+using Net6WebApiTemplate.Application.Common.Interfaces;
+using Net6WebApiTemplate.Infrastructure.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Net6WebApiTemplate.Infrastructure.Oauth
+{
+    public class JwtTokenManager : IJwtTokenManager
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly TokenValidationParameters _tokenValidationParameters;
+
+        public JwtTokenManager(JwtSettings jwtSettings, UserManager<ApplicationUser> userManager, TokenValidationParameters tokenValidationParameters)
+        {
+            _jwtSettings = jwtSettings;
+            _userManager = userManager;
+            _tokenValidationParameters = tokenValidationParameters;
+        }
+
+        public async Task<AuthResult> GenerateClaimsTokenAsync(string username, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+
+                    new Claim(ClaimTypes.NameIdentifier, user.Id), // TODO: encrypt user id for added security
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(JwtRegisteredClaimNames.Sub, username),
+                    new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddMinutes(5)).ToUnixTimeSeconds().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+
+                }),
+                Expires = DateTime.UtcNow.Add(_jwtSettings.Expiration),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return new AuthResult
+            {
+                AccessToken = tokenHandler.WriteToken(token),
+                TokenType = "Bearer",
+                ExpiresIn = _jwtSettings.Expiration.Seconds,
+                RefreshToken = ""
+            };
+        }
+
+        public async Task<ClaimsPrincipal> GetPrincipFromTokenAsync(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                // disable token lifetime validation as we are validating against an expired token.
+                var tokenValdationParams = _tokenValidationParameters.Clone();
+                tokenValdationParams.ValidateLifetime = false;
+
+                var principal = tokenHandler.ValidateToken(token, tokenValdationParams, out var validatedToken);
+                if (!IsJwtWithValidSecurityAlgorithm(validatedToken))
+                {
+                    return null;
+                }
+
+                return await Task.Run(() => principal);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static bool IsJwtWithValidSecurityAlgorithm(SecurityToken validatedToken)
+        {
+            return (validatedToken is JwtSecurityToken jwtSecurityToken) &&
+                jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
